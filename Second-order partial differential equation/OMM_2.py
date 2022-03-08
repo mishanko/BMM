878e23f35009537154ac642b5bc0f188a470c5c7
Mikhail <mishajudoist@gmail.com>
2022-03-08 21:00:26 +0300

    added solution of simpler equation

--- Second-order partial differential equation/OMM_2.py
+++ Second-order partial differential equation/OMM_2.py
@@ -1,126 +1,108 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math as mt
-from mpl_toolkits.mplot3d import axes3d
 from tqdm import tqdm
-import sys
-
-# * Объявим некоторые 
-L, T = mt.pi, mt.pi # * x in (0, L) и длина расчетного промежутка по времени 
-H = 3 # * y in (0, H) 
-N = 101 # * число узлов по x 
-M = 101 # * число узлов по y 
-
-hx = L / (N-1) # * шаг по x 
-hy = H / (M-1) # * шаг по y 
-
-# J = 2 + round(2 * T * mt.sqrt(1 / hx ** 2 + 1 / hy ** 2))
-J = 101
-
-tau =  T / (J-1) # * шаг по t 
-x = np.zeros((N, )) # * массив узлов по x 
-y = np.zeros((M, )) # * массив узлов по y 
-t = np.zeros((J, )) # * массив узлов по t 
-
-# * Задание сетки---------------------------------------------------- 
-
-for n in range(N):
-    x[n] = n * hx / 2 - (n + 1) * hx
-for m in range(M):
-    y[m] = m * hy / 2 - (m + 1) * hy
-for j in range(J):
-    t[j] = j * tau
-
-# * ------------------------------------------------------------------ 
-# *  Аналитическое решение-------------------------------------------- 
-
-u = np.zeros((M, N, J))
-for j in range(J):
-    for n in range(N):
-        for m in range(M):
-            u[m, n, j] = (np.exp(-4 * t[j]) + (1 / 17) * (4 * np.sin(t[j]) - np.cos(t[j]) + np.exp(-4 * t[j]))) * np.cos(x[n]) * np.cos(y[m]/3)
 
 
-# * ----------------------------------------------------------------- 
-# * Численное решение------------------------------------------------ 
+Nx = 160 # число шагов по x
+Ny = 160 # число шагов по y
+M = 100 #число шагов по t
 
-v, err = np.zeros((M, N, J)), np.zeros((M, N, J)) # * численное решение и погрешность 
+x = np.linspace(0, 1, Nx)
+y = np.linspace(0, 1, Ny)
+t = np.linspace(0, 1, M)
 
-# * Начальные условия:
-for n in range(N):
-    for m in range(M):
-        v[m, n, 0] = np.cos(x[n])
+h_x = x[1] - x[0]
+h_y = y[1] - y[0]
+tau = t[1] - t[0]
 
-w = np.zeros((M, N)) # * вспомогательная функция при переходе со слоя на слой 
-alpha_x=np.zeros((N-1, )) 
-beta_x=np.zeros((N-1, )) 
-alpha_y=np.zeros((M-1, )) 
-beta_y=np.zeros((M-1, )) 
-Ay = 2 * tau / hy**2
-By = 2 * tau / hy**2
-Ax = 2 * tau / hx**2
-Bx = 2 * tau / hx**2
-Cx = (1 + 4 * tau / hx**2) 
-Cy = (1 + 4 * tau / hy**2)
+gamma_x = tau / h_x**2
+gamma_y = tau / h_y**2
 
-for j in tqdm(range(J-1)):
-    # * вычисление вспомогательной функции w ?----------------------- 
-    for m in range(1, M-1):
-        alpha_x[0] = 1
-        # * прямой ход прогонки по x--------------------------------- 
-        for n in range(1, N-1):
-            F = 1 / 2 * (v[m-1, n, j] - 2 * v[m, n, j] + v[m+1, n, j]) * 4 * tau / hx**2 + v[m, n, j] * 4 + 2 * tau * np.cos(x[n]) * np.sin(t[j] + tau / 2)
-            alpha_x[n] = Bx / (Cx - Ax * alpha_x[n-1])
-            beta_x[n] = (F + Ax * beta_x[n-1]) / (Cx - Ax * alpha_x[n-1])
+def lam(k):
+    return (np.pi ** 2) * (1 + (2 * k + 1) ** 2)
 
-        # * обратный ход прогонки по x-------------------------------
-        w[m, N-1] = beta_x[N-2] / (1 - alpha_x[N-2])
-        for n in range(N-2, -1, -1):
-            w[m, n] = alpha_x[n] * w[m, n+1] + beta_x[n]
+def exact(x, y, t):
+    z = 0
+    for k in range(10):
+        z = z + (np.sin((np.pi * x) * (2 * k + 1)) * (np.exp(t) - np.exp(-lam (k) * t))) / ((2 * k + 1) * np.pi * (lam(k) + 1))
+    return 4 * np.cos(np.pi * y) * z
 
-        w[m, N-1] = w[m, N-2]
-        w[m, 0] = w[m, 1]
-        # * вычисление функции v на новом слое по времени------------ 
-        alpha_y[0] = 1
 
-    for n in range(1, N-1):
-        
-        # * прямой ход прогонки по y--------------------------------- 
-        for m in range(1, M-1):
-            F = 1 / 2 * (w[m, n-1] - 2 * w[m, n] + w[m, n+1]) * 4 * tau / hy**2 + w[m, n] * 4 + 2 * tau * np.cos(x[n]) * np.sin(t[j] + tau / 2) 
-            alpha_y[m] = By / (Cy - Ay * alpha_y[m-1])
-            beta_y[m] = (F + Ay * beta_y[m-1]) / (Cy - Ay * alpha_y[m-1])
-            
-        # * обратный ход прогонки по y-------------------------------
-        v[M-1, n, j+1] = beta_y[M-2] / (1 - alpha_y[M-2])
-        err[M-1, n, j+1] = v[M-1, n, j+1] - u[M-1, n, j+1]
-        for m in range(M-2, -1, -1):
-            v[m, n, j+1] = alpha_y[m] * v[m+1, n, j+1] + beta_y[m]
-            err[m, n, j+1] = v[m, n, j+1] - u[m, n, j+1]
+u = np.zeros((Nx, Ny, 2 * M + 1))
 
-    # * граничные условия:
-    for m in range(M):
-        v[m, 0, j+1] = v[m, 1, j+1]
-        err[m, 0, j+1] = v[m, 0, j+1] - u[m, 0, j+1]
-        v[m, N-1, j+1] = v[m, N-2, j+1]
-        err[m, N-1, j+1] = v[m, N-1, j+1] - u[m, N-1, j+1]
+def F_1(i1, i2, j):
+    return 0.5 * gamma_y * (u[i1, i2-1, j-1] + u[i1, i2+1, j-1]) + (1 - gamma_y) * u[i1, i2, j-1] + 0.5 * tau * np.exp(tau * (j + 1 / 2)) * np.cos(np.pi * y[i2])
 
-    for n in range(N):
-        v[0, n, j+1] = v[1, n, j+1]
-        err[0, n, j+1] = v[0, n, j+1] - u[0, n, j+1]
-        v[M-1, n, j+1] = v[M-2, n, j+1] 
-        err[M-1, n, j+1] = v[M-1, n, j+1] - u[M-1, n, j+1]
+def F_2(i1, i2, j):
+    return 0.5 * gamma_x * (u[i1-1, i2, j-1] + u[i1+1, i2, j-1]) + (1 - gamma_x) * u[i1, i2, j-1] + 0.5 * tau * np.exp(tau * (j + 1 / 2)) * np.cos(np.pi * y[i2])
 
-j_ = 20
+def progonka_x(i2, j):
 
-ym = np.linspace(0, H, num=M)
-xn = np.linspace(0, L, num=N)
+    d = np.zeros(Nx)
+    sigma = np.zeros(Nx)
+    d[1] = 0
+    sigma[1] = 0
+    A = 0.5 * gamma_x
+    B = 1 + gamma_x
+    C = 0.5 * gamma_x
+
+    for m in range(1, Nx-1):
+        Fm = -F_1(m, i2, j)
+        d[m+1] = C / (B - A * d[m])
+        sigma[m+1] = (Fm - A * sigma[m]) / (A * d[m] - B)
+    u[Nx-1, i2, j] = 0
+    u[0, i2, j] = 0
+
+    for m in range(Nx-1, 0, -1):
+        u[m-1, i2, j] = d[m] * u[m, i2, j] + sigma[m]
+
+def progonka_y(i1, j):
+
+    d = np.zeros(Ny)
+    sigma = np.zeros(Ny)
+    d[1] = 1
+    sigma[1] = 0
+    A = 0.5 * gamma_y
+    B = 1 + gamma_y
+    C = 0.5 * gamma_y
+    
+    for m in range(1, Ny-1):
+        Fm = -F_2(i1, m, j)
+        d[m+1] = C / (B - A * d[m])
+        sigma[m+1] = (Fm - A * sigma[m]) / (A * d[m] - B)
+    u[i1, Ny - 1, j] = sigma[-1] / (1 - d[-1])
+
+    for m in range(Ny-1, 0, -1):
+        u[i1, m-1, j] = d[m] * u[i1, m, j] + sigma[m]
+
+u[:, :, 0] = 0 #начальное условие
+
+for j in tqdm(range(1, 2 * M, 2)): #выполнение расчетов
+    for i2 in range(1, Ny - 1):
+        progonka_x(i2, j)
+    for i1 in range(1, Nx - 1):
+        progonka_y(i1, j+1)
+
+j_ = 80
+
+p = np.zeros((Nx, Ny))
+def p_(j_):
+    for i in tqdm(range(Nx)):
+        for j in range(Ny):
+            p[i][j] = exact(i * h_x, j * h_y, j_ * tau)
+    return p
+
+ym = np.linspace(0, 1, num=Ny)
+xn = np.linspace(0, 1, num=Nx)
 X, Y = np.meshgrid(xn, ym)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-print(v.shape, X.shape, Y.shape, v[:, :, j_].shape)
-surf_1 = ax.plot_wireframe(X, Y, v[:, :, j_], rstride=10, cstride=1)
-surf_2 = ax.plot_wireframe(X, Y, u[:, :, j_], rstride=10, cstride=1, color='r')
+print(p.shape, X.shape, Y.shape)
+nnn = p_(j_)[:, :]
+surf_1 = ax.plot_wireframe(X, Y, nnn, rstride=10, cstride=1, color='r')
+surf_2 = ax.plot_wireframe(X, Y, u[:, :, j_], rstride=10, cstride=1)
+surf_3 = ax.plot_wireframe(X, Y, np.abs(nnn - u[:, :, j_]), rstride=10, cstride=1, color='g')
+print(np.mean(np.abs(nnn - u[:, :, j_])))
 
 plt.title('Решение')
 plt.xlabel('X')
--- Second-order partial differential equation/new_OMM_2.py
+++ Second-order partial differential equation/new_OMM_2.py
@@ -11,116 +11,111 @@ H = 3 # * y in (0, H)
 N = 100 # * число узлов по x 
 M = 100 # * число узлов по y 
 
-hx = L / N # * шаг по x 
-hy = H / M # * шаг по y 
+hx = L / (N-1) # * шаг по x 
+hy = H / (M-1) # * шаг по y 
 
 # J = 2 + round(2 * T * mt.sqrt(1 / hx ** 2 + 1 / hy ** 2))
 J = 100
 
-tau =  T / J # * шаг по t 
-x = np.zeros((N, )) # * массив узлов по x 
-y = np.zeros((M, )) # * массив узлов по y 
-t = np.zeros((J, )) # * массив узлов по t 
+tau =  T / (J-1) # * шаг по t 
 
 # * Задание сетки---------------------------------------------------- 
 
-for n in range(N):
-    x[n] = n * hx / 2 - (n + 1) * hx
-for m in range(M):
-    y[m] = m * hy / 2 - (m + 1) * hy
-for j in range(J):
-    t[j] = j * tau
+x = np.linspace(0, L, N-1)
+y = np.linspace(0, H, M-1)
+t = np.linspace(0, T, J-1)
 
 # * ------------------------------------------------------------------ 
 # *  Аналитическое решение-------------------------------------------- 
 
-u = np.zeros((M, N, J))
-for j in range(J):
-    for n in range(N):
-        for m in range(M):
-            u[m, n, j] = (np.exp(-4 * t[j]) + 1/17 * (4 * np.sin(t[j]) - np.cos(t[j]) + np.exp(-4 * t[j]))) * np.cos(x[n]) * np.cos(y[m]/3)
-
+u = np.zeros((N, M, J))
+for j in range(J-1):
+    for n in range(N-1):
+        for m in range(M-1):
+            u[n, m, j] = (np.exp(-4 * t[j]) + 1/17 * 
+                          (4 * np.sin(t[j]) - np.cos(t[j]) + np.exp(-4 * t[j]))) * \
+                            np.cos(x[n]) * np.sin(y[m] * mt.pi / 3)
 
 # * ----------------------------------------------------------------- 
 # * Численное решение------------------------------------------------ 
 
-v, err, Fx, Fy = np.zeros((M, N, J)), np.zeros((M, N, J)), np.zeros((M, N, J)), np.zeros((M, N, J)) # * численное решение и погрешность 
+v, err = np.zeros((N, M, J)), np.zeros((N, M, J)) # * численное решение и погрешность 
 
 # * Начальные условия:
-for n in range(N):
-    for m in range(M):
-        v[m, n, 0] = np.cos(x[n])
+for n in range(N-1):
+    for m in range(M-1):
+        v[n, m, 0] = np.cos(x[n])
 
-alpha_x=np.zeros((M, N)) 
-beta_x=np.zeros((M, N)) 
-alpha_y=np.zeros((M, N)) 
-beta_y=np.zeros((M, N)) 
-Ay = 2 * tau / hy**2
-By = 2 * tau / hy**2
-Ax = 2 * tau / hx**2
-Bx = 2 * tau / hx**2
-Cx = (1 + 4 * tau / hx**2) 
-Cy = (1 + 4 * tau / hy**2)
+gamma_x = 4 * tau / hx ** 2
+gamma_y = 4 * tau / hy ** 2
 
-for j in tqdm(range(J-1)):
-    # * граничные условия:
-    for m in range(M):
-        v[m, 0, j] = v[m, 1, j]
-        err[m, 0, j] = v[m, 0, j] - u[m, 0, j]
-        v[m, N-1, j] = v[m, N-2, j]
-        err[m, N-1, j] = v[m, N-1, j] - u[m, N-1, j]
+def F_1(n, m, j):
+    return 0.5 * gamma_y * (v[n, m-1, j-1] + v[n, m+1, j-1]) + (1 - gamma_y) * v[n, m, j-1] + 2 * tau * np.cos(x[n]) * np.sin(tau*((j + 1) / 2))
 
-    for n in range(N):
-        v[0, n, j] = v[1, n, j]
-        err[0, n, j] = v[0, n, j] - u[0, n, j]
-        v[M-1, n, j] = v[M-2, n, j] 
-        err[M-1, n, j] = v[M-1, n, j] - u[M-1, n, j]
+def F_2(n, m, j):
+    return 0.5 * gamma_x * (v[n-1, m, j-1] + v[n+1, m, j-1]) + (1 - gamma_x) * v[n, m, j-1] + 2 * tau * np.cos(x[n]) * np.sin(tau*((j + 1) / 2))
+
+def progonka_x(m, j):
+    d = np.zeros(N)
+    sigma = np.zeros(N)
+    d[1] = 0
+    sigma[1] = 1
+    A = 0.5 * gamma_x
+    B = 1 + gamma_x
+    C = 0.5 * gamma_x
+
+    for n in range(1, N-1):
+        Fm = -F_1(n, m, j)
+        d[n+1] = C / (B - A * d[n])
+        sigma[n+1] = (Fm + A * sigma[n]) / (B - A * d[n])
+
+    v[N-1, m, j] = sigma[N-2] / (1 - d[N-2])
+    
+    for n in range(N-1, 0, -1):
+        v[n-1, m, j] = d[n] * v[n, m, j] + sigma[n]
+        err[n-1, m, j] = v[n-1, m, j] - u[n-1, m, j]
         
-    # * прямой ход прогонки по x---------------------------------     
-    for n in range(1, N-1):
-        for m in range(1, M-1):
-            Fx[m, n, j] = 1 / 2 * (v[m-1, n, j] - 2 * v[m, n, j] + v[m+1, n, j]) * 4 * tau / hx**2 + v[m, n, j] * 4 + 2 * tau * np.cos(x[n]) * np.sin(t[j] + tau / 2)
+def progonka_y(n, j):
+    d = np.zeros(M)
+    sigma = np.zeros(M)
+    d[1] = 0
+    sigma[1] = 1
+    A = 0.5 * gamma_y
+    B = 1 + gamma_y
+    C = 0.5 * gamma_y
 
-    for m in range(1, M-1):   
-        alpha_x[m, 1] = 1
-        beta_x[m, 1] = 0   
-        for n in range(1, N-1):
-            alpha_x[m, n+1] = Bx / (Cx - Ax * alpha_x[m, n])
-            beta_x[m, n+1] = (Fx[m, n, j] + Ax * beta_x[m, n]) / (Cx - Ax * alpha_x[m, n])
-
-    # * обратный ход прогонки по x-------------------------------
     for m in range(1, M-1):
-        v[m, N-1, j] = v[m, N-2, j]
-        for n in range(N-2, -1, -1):
-            v[m, n, j] = alpha_x[m, n+1] * v[m, n+1, j] + beta_x[m, n+1]
-            err[m, n, j] = v[m, n, j] - u[m, n, j]
+        Fm = -F_2(n, m, j)
+        d[m+1] = C / (B - A * d[m])
+        sigma[m+1] = (Fm + A * sigma[m]) / (B - A * d[m])
+        
+    v[n, M-1, j] = sigma[M-2] / (1 - d[M-2])
+    err[n, M-1, j] = v[n, M-1, j] - u[n, M-1, j]
+    for m in range(M-1, 0, -1):
+        v[n, m-1, j] = d[m] * v[n, m, j] + sigma[m]
+        err[n, m-1, j] = v[n, m-1, j] - u[n, m-1, j]
 
-    # ------------------------------------------------------------------------------
 
-    # * прямой ход прогонки по y---------------------------------     
+for j in tqdm(range(1, J-1)): #выполнение расчетов
     for m in range(1, M-1):
-        for n in range(1, N-1):
-            Fy[m, n, j] = 1 / 2 * (v[m, n-1, j] - 2 * v[m, n, j] + v[m, n+1, j]) * 4 * tau / hy**2 + v[m, n, j] * 4 + 2 * tau * np.cos(x[n]) * np.sin(t[j] + tau / 2)
-
-    for n in range(1, N-1):   
-        alpha_y[1, n] = 1
-        beta_y[1, n] = 0   
-        for m in range(1, M-1):
-            alpha_y[m+1, n] = By / (Cy - Ay * alpha_y[m, n])
-            beta_y[m+1, n] = (Fy[m, n, j] + Ay * beta_y[m, n]) / (Cy - Ay * alpha_y[m, n])
-
-    # * обратный ход прогонки по y-------------------------------
+        progonka_x(m, j)
     for n in range(1, N-1):
-        v[M-1, n, j] = v[M-2, n, j]
-        for m in range(M-2, -1, -1):
-            v[m, n, j] = alpha_y[m+1, n] * v[m+1, n, j] + beta_y[m+1, n]
-            err[m, n, j] = v[m, n, j] - u[m, n, j]
+        progonka_y(n, j+1)
+        
+    # * граничные условия:
+    for n in range(N):
+        v[n, 1, j] = v[n, 0, j]
+        err[n, 0, j] = v[n, 0, j] - u[n, 0, j]
+        v[n, M-2, j] = v[n, M-1, j]
+        err[n, M-1, j] = v[n, M-1, j] - u[n, M-1, j]
 
-    
+    for m in range(M):
+        v[1, m, j] = v[0, m, j]
+        err[0, m, j] = v[0, m, j] - u[0, m, j]
+        v[N-2, m, j] = v[N-1, m, j] 
+        err[N-1, m, j] = v[N-1, m, j] - u[N-1, m, j]
 
-    
-
-j_ = 30
+j_ = 10
 
 ym = np.linspace(0, H, num=M)
 xn = np.linspace(0, L, num=N)
